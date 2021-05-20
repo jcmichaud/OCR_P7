d86885a22ca3ebea2e2de5e0a9f42e64f5487929
jcmichaud <48595583+jcmichaud@users.noreply.github.com>
2021-05-20 18:18:53 +0200

    v0.003

--- Dashboard.py
+++ Dashboard.py
@@ -17,44 +17,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8'))
-                ,sep=",")
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing the file:' + filename + '.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
+train = pd.read_csv("Data/train20may21.csv")
 
 result_assessment = dcc.Graph(
         id='result_assessment',
@@ -76,25 +39,6 @@ slider_revenu=dcc.RangeSlider(id='slider_revenu',
                             },
                     value=[1,8])
 
-upload_files = dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    )
 
 
 app.layout = html.Div(
@@ -113,10 +57,7 @@ app.layout = html.Div(
         html.Tr([html.Td(['Age Range Selected']), html.Td(id='age_range_output')]),
         html.Tr([html.Td(['Revenu Range Selected']), html.Td(id='revenu_range_output')]),
     ]), 
-    html.Hr(), 
-    upload_files,
-
-    html.Div(id='output-data-upload'),
+    html.Hr()
     ],
     style={'columnCount': 3,
     "background-color":'white',
@@ -143,24 +84,6 @@ def update_output_div(input_value):
 def update_output_div(input_value):
     return 'Revenu selected: {}'.format(input_value)
 #########################################
-
-
-######################   uploading file  
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-#########################################
-
-
-
 
 
 if __name__ == '__main__':
